e2095561ad349c27fdb362f8aabfdf9bec4f19ae
Leonard Nickels <leonard.nickels@student.fhws.de>
2017-04-13 15:53:35 +0200

    Initial Commit

---
+++ app/src/main/java/todo/spielesammlungprototyp/Brettspiele.java
@@ -0,0 +1,13 @@
+package todo.spielesammlungprototyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Brettspiele extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brettspiele);
+    }
+}
