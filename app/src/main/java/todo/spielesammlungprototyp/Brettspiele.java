4eae0ca75b81a46f64998d62f6cb2666fb6fc9bd
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-04-17 23:01:58 +0200

    Show toast message and clear input on button press

--- app/src/main/java/todo/spielesammlungprototyp/Brettspiele.java
+++ app/src/main/java/todo/spielesammlungprototyp/Brettspiele.java
@@ -2,7 +2,10 @@ package todo.spielesammlungprototyp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class Brettspiele extends AppCompatActivity {
 
@@ -26,5 +29,18 @@ public class Brettspiele extends AppCompatActivity {
                 scrollView.fullScroll(ScrollView.FOCUS_DOWN);
             }
         });
+    }
+
+    public void confirmInput(View view) {
+        final EditText input_console = (EditText) findViewById(R.id.input_console);
+        String input = input_console.getText().toString();
+
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+        clearInput();
+    }
+
+    private void clearInput() {
+        final EditText editText = (EditText) findViewById(R.id.input_console);
+        editText.getText().clear();
     }
 }
