8b094ede68c67be4188290949882f7ebbc54fbea
Oliver <oilG@hotmail.de>
2017-04-18 21:21:33 +0200

    define exeptions of Chesstest.move

--- app/src/main/java/todo/spielesammlungprototyp/Chess/Chesstest.java
+++ app/src/main/java/todo/spielesammlungprototyp/Chess/Chesstest.java
@@ -1,5 +1,6 @@
 package todo.spielesammlungprototyp.Chess;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import chesspresso.position.*;
@@ -25,7 +26,7 @@ public class Chesstest {
         position.doMove(move);
     }
 
-    public void move(String from,String to) throws Exception {
+    public void move(String from,String to) throws ClassNotFoundException,NoSuchMethodException,InvocationTargetException,IllegalAccessException,IllegalMoveException{
         Class<?> c = Class.forName("Chess");
         Method methodfrom = c.getDeclaredMethod("from");
         Method methodto = c.getDeclaredMethod("from");
