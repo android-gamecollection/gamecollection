669cde173e75592b7cbd230af4012156290cd4d5
Oliver <oilG@hotmail.de>
2017-04-18 21:04:43 +0200

    add move functionality to chesstest

--- app/src/main/java/todo/spielesammlungprototyp/Chess/Chesstest.java
+++ app/src/main/java/todo/spielesammlungprototyp/Chess/Chesstest.java
@@ -1,8 +1,11 @@
 package todo.spielesammlungprototyp.Chess;
 
+import java.lang.reflect.Method;
+
 import chesspresso.position.*;
 import chesspresso.move.*;
 import chesspresso.Chess;
+
 
 
 public class Chesstest {
@@ -18,6 +21,24 @@ public class Chesstest {
 
     public void testmove() throws Exception {
         short move = Move.getRegularMove(Chess.E2, Chess.E4, false);
+        position.doMove(move);
+    }
+    public void move(String from,String to)throws Exception {
+
+        Class<?> c = Class.forName("Chess");
+        Method methodfrom = c.getDeclaredMethod("from");
+        Method methodto = c.getDeclaredMethod("from");
+        int f = 0;
+        int t = 0;
+        methodfrom.invoke(f);
+        methodto.invoke(t);
+        boolean capturing = false;
+        if(!position.isSquareEmpty(t))
+        {
+            capturing = true;
+        }
+        short move = Move.getRegularMove(f,t,capturing);
+
         position.doMove(move);
     }
 }
