b999d13a63b16bd08e37c86364845ee35c0df9e6
PhilippJung <phil2397@googlemail.com>
2017-04-19 11:16:32 +0200

    Added GotoBauernkrieg Method

--- app/src/main/java/todo/spielesammlungprototyp/Kartenspiele.java
+++ app/src/main/java/todo/spielesammlungprototyp/Kartenspiele.java
@@ -1,7 +1,9 @@
 package todo.spielesammlungprototyp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Kartenspiele extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Kartenspiele extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kartenspiele);
+    }
+    public void gotoBauernkrieg(View view) {
+        Intent gotoActivity = new Intent(this, todo.spielesammlungprototyp.Bauernkrieg.src.app.bauernkrieg.Bauernkrieg.class);
+        startActivity(gotoActivity);
     }
 }
