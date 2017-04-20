d29a9c68dc13b06c2c3cdc8b4cf91eebd3f5d08b
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-04-20 09:33:30 +0200

    Add command 'overview'
    Console uses monospace typeface
    Remove output after move command

--- app/src/main/java/todo/spielesammlungprototyp/Chess/ChessBoard.java
+++ app/src/main/java/todo/spielesammlungprototyp/Chess/ChessBoard.java
@@ -18,6 +18,19 @@ class ChessBoard {
         return position.toString();
     }
 
+    String getOverview() {
+        String str = "";
+        for (int i = 0; i < 64; i++) {
+            // 56, 57, .., 63, 48, 49, ..., 0, 1, 2, .., 7
+            int positionNumber = i + 56 - (i / 8) * 16;
+            int piece = position.getPiece(positionNumber);
+            char pieceChar = piece != 0 ? Chess.pieceToChar(piece) : '-';
+            // Newline every 8 fields
+            str += pieceChar + ((i + 1) % 8 == 0 ? "\n" : "");
+        }
+        return str;
+    }
+
     void move(String from, String to) throws IllegalMoveException{
         short move = Move.getRegularMove(Chess.strToSqi(from),Chess.strToSqi(to),false);
         position.doMove(move);
