581463a6b28342f0ae04c5490da95b77fd5970e6
Oliver Gawron <oliver.gawron@student.fhws.de>
2017-04-23 00:28:50 +0200

    Implement carballo and catch invalid moves

--- app/src/main/java/todo/spielesammlungprototyp/Chess/ChessBoard.java
+++ app/src/main/java/todo/spielesammlungprototyp/Chess/ChessBoard.java
@@ -1,39 +1,33 @@
 package todo.spielesammlungprototyp.Chess;
 
-import chesspresso.position.*;
-import chesspresso.move.*;
-import chesspresso.Chess;
-
-
+import com.alonsoruibal.chess.*;
 
 class ChessBoard {
 
-    private Position position;
+    private Board board;
+    public ChessBoard()
+    {
+        board = new Board();
+    }
 
     void startPosition() {
-        position = Position.createInitialPosition();
+        board.startPosition();
     }
 
     String getBoard() {
-        return position.toString();
+        return board.getFen();
     }
 
     String getOverview() {
-        String str = "";
-        for (int i = 0; i < 64; i++) {
-            // 56, 57, .., 63, 48, 49, ..., 0, 1, 2, .., 7
-            int positionNumber = i + 56 - (i / 8) * 16;
-            int piece = position.getPiece(positionNumber);
-            char pieceChar = piece != 0 ? Chess.pieceToChar(piece) : '-';
-            // Newline every 8 fields
-            str += pieceChar + ((i + 1) % 8 == 0 && i != 63 ? "\n" : " ");
-        }
+        String str = board.toString();
+        str = str.substring(0,144);// 144 = amount of chars on a normal field
+        str = str + "\n" + "a b c d e f g h";
         return str;
     }
 
-    void move(String from, String to) throws IllegalMoveException{
-        short move = Move.getRegularMove(Chess.strToSqi(from),Chess.strToSqi(to),false);
-        position.doMove(move);
+    boolean move(String from, String to){
+        int move = Move.getFromString(board,from + " " + to,true);
+        return board.doMove(move);
 
     }
 }
