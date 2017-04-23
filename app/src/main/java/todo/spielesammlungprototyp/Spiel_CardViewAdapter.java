5190bdbfa5514ad8e9d60c9662bb2fb8d04e0473
Nickels, Leonard <leonard.nickels@student.fhws.de>
2017-04-23 16:02:23 +0200

    added "Kartenspiele_Auswahl" activtiy with CardView and RecyclerView
    added switchcase for both "..._auswahl" layouts in "Spiel_CardViewAdapter" class
    added "activity_kartenspiele_auswahl.xml" and "activity_kartenspiele_auswahl_card_view.xml" layouts
    all for CardView usage

--- app/src/main/java/todo/spielesammlungprototyp/Spiel_CardViewAdapter.java
+++ app/src/main/java/todo/spielesammlungprototyp/Spiel_CardViewAdapter.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 
 class Spiel_CardViewAdapter extends RecyclerView.Adapter<Spiel_CardViewAdapter.SpieleViewHolder>{
 
+    private static final int VIEW_TYPE_FIRST  = 0;
+    private static final int VIEW_TYPE_SECOND = 1;
     private ArrayList<Spiel_CardView> spiele_cardview = new ArrayList<>();
 
     Spiel_CardViewAdapter(ArrayList<Spiel_CardView> spiele_cardview) {
@@ -19,8 +21,17 @@ class Spiel_CardViewAdapter extends RecyclerView.Adapter<Spiel_CardViewAdapter.S
 
     @Override
     public SpieleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_brettspiele_auswahl_card_view, parent, false);
-        return new SpieleViewHolder(view);
+        switch (viewType) {
+            case VIEW_TYPE_FIRST: {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_brettspiele_auswahl_card_view, parent, false);
+                return new SpieleViewHolder(view);
+            }
+            case VIEW_TYPE_SECOND: {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_kartenspiele_auswahl_card_view, parent, false);
+                return new SpieleViewHolder(view);
+            }
+        }
+        return null;
     }
 
     @Override
