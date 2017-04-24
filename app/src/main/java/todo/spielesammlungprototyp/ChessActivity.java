59535809a90324a3298b34ef63b92613ec1b4fbe
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-04-25 00:48:05 +0200

    Replace static chess pieces generating with 'setFromFen' method
    Method takes any FEN string and converts it to show in the GridLayout
    TODO: possiblity to update layout

--- app/src/main/java/todo/spielesammlungprototyp/ChessActivity.java
+++ app/src/main/java/todo/spielesammlungprototyp/ChessActivity.java
@@ -7,87 +7,19 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.alonsoruibal.chess.Board;
 import todo.spielesammlungprototyp.Chess.ChessGrid;
 import todo.spielesammlungprototyp.Chess.ucChessPiece;
 
 public class ChessActivity extends Activity {
+
     GridView grid;
 
     int[] imageId = new int[64];
-
-    String[] chessFigure = {
-            ucChessPiece.ROOK_B,
-            ucChessPiece.KNIGHT_B,
-            ucChessPiece.BISHOP_B,
-            ucChessPiece.QUEEN_B,
-            ucChessPiece.KING_B,
-            ucChessPiece.BISHOP_B,
-            ucChessPiece.KNIGHT_B,
-            ucChessPiece.ROOK_B,
-
-            ucChessPiece.PAWN_B,
-            ucChessPiece.PAWN_B,
-            ucChessPiece.PAWN_B,
-            ucChessPiece.PAWN_B,
-            ucChessPiece.PAWN_B,
-            ucChessPiece.PAWN_B,
-            ucChessPiece.PAWN_B,
-            ucChessPiece.PAWN_B,
-
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-            ucChessPiece.NONE,
-
-            ucChessPiece.PAWN_W,
-            ucChessPiece.PAWN_W,
-            ucChessPiece.PAWN_W,
-            ucChessPiece.PAWN_W,
-            ucChessPiece.PAWN_W,
-            ucChessPiece.PAWN_W,
-            ucChessPiece.PAWN_W,
-            ucChessPiece.PAWN_W,
-
-            ucChessPiece.ROOK_W,
-            ucChessPiece.KNIGHT_W,
-            ucChessPiece.BISHOP_W,
-            ucChessPiece.QUEEN_W,
-            ucChessPiece.KING_W,
-            ucChessPiece.BISHOP_W,
-            ucChessPiece.KNIGHT_W,
-            ucChessPiece.ROOK_W,
-    };
+    String[] chessFigure = new String[64];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,16 +27,26 @@ public class ChessActivity extends Activity {
         setContentView(R.layout.chess_grid);
 
         setGridColors();
-        ChessGrid adapter = new ChessGrid(ChessActivity.this, imageId, chessFigure);
-        grid=(GridView)findViewById(R.id.gridview_Chess);
-            grid.setAdapter(adapter);
-            grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        setFromFen(Board.FEN_START_POSITION);
 
+        ChessGrid adapter = new ChessGrid(ChessActivity.this, imageId, chessFigure);
+        grid = (GridView)findViewById(R.id.gridview_Chess);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(ChessActivity.this, "You Clicked at "+imageId[position], Toast.LENGTH_SHORT).show();
+                Toast.makeText(ChessActivity.this, "You Clicked at " + imageId[position], Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void setFromFen(String fen) {
+        fen = fen.split("\\s+")[0];
+        fen = replaceNumbers(fen);
+        fen = fen.replace("/", "");
+
+        for (int i = 0; i < 64; i++)
+            chessFigure[i] = ucChessPiece.pieces.get(fen.charAt(i));
     }
 
     private void setGridColors() {
@@ -112,5 +54,19 @@ public class ChessActivity extends Activity {
             boolean testable = (i + i/8 % 2) % 2 == 0;
             imageId[i] = testable ? R.drawable.chess_white_plate : R.drawable.chess_black_plate;
         }
+    }
+
+    private String replaceNumbers(String str) {
+        Pattern pattern = Pattern.compile("(\\d)");
+        Matcher matcher = pattern.matcher(str);
+        StringBuffer s = new StringBuffer();
+        while (matcher.find())
+            matcher.appendReplacement(s, genSpace(Integer.parseInt(matcher.group(1))));
+        matcher.appendTail(s);
+        return s.toString();
+    }
+
+    private String genSpace(int length) {
+        return new String(new char[length]).replace("\0", "_");
     }
 }
