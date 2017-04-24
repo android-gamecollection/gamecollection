58aebdaf999a89d91b30ec3443260392f5a980e9
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-04-24 22:56:11 +0200

    Change generation of grid colors and chess pieces
    Add class 'ucChessPiece' with unicode constants of chess pieces

---
+++ app/src/main/java/todo/spielesammlungprototyp/Chess/ucChessPiece.java
@@ -0,0 +1,19 @@
+package todo.spielesammlungprototyp.Chess;
+
+public abstract class ucChessPiece {
+    public static final String BISHOP_B = "\u265D";
+    public static final String KING_B = "\u265A";
+    public static final String KNIGHT_B = "\u265E";
+    public static final String PAWN_B = "\u265F";
+    public static final String ROOK_B = "\u265C";
+    public static final String QUEEN_B = "\u265B";
+
+    public static final String BISHOP_W = "\u2657";
+    public static final String KING_W = "\u2654";
+    public static final String KNIGHT_W = "\u2658";
+    public static final String PAWN_W = "\u2659";
+    public static final String ROOK_W = "\u2656";
+    public static final String QUEEN_W = "\u2655";
+
+    public static final String NONE = "";
+}
