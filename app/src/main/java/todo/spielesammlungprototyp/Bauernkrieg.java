400f71ea65011029eb1da074851fb92a6e478b57
Leonard Nickels <leonard.nickels@student.fhws.de>
2017-04-29 12:57:08 +0200

    Added transitions to every important activity

--- app/src/main/java/todo/spielesammlungprototyp/Bauernkrieg.java
+++ app/src/main/java/todo/spielesammlungprototyp/Bauernkrieg.java
@@ -2,6 +2,7 @@ package todo.spielesammlungprototyp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Bauernkrieg extends Activity {
 
@@ -9,5 +10,22 @@ public class Bauernkrieg extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bauernkrieg);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_out_right, R.anim.slide_in_left);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                overridePendingTransition(R.anim.slide_out_right, R.anim.slide_in_left);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
