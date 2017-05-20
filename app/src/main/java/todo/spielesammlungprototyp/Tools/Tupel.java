5e5109425736f6c97745dbc8678a9e261064f70c
Oliver Gawron <oliver.gawron@student.fhws.de>
2017-05-20 18:57:56 +0200

    add package "Tools" and add Class Tupel

---
+++ app/src/main/java/todo/spielesammlungprototyp/Tools/Tupel.java
@@ -0,0 +1,16 @@
+package todo.spielesammlungprototyp.Tools;
+
+/**
+ * Created by Oliver on 20.05.2017.
+ */
+
+public class Tupel<T,F> {
+
+    public T first;    //X
+    public F second;   //Y
+
+    public Tupel(T first, F second) {
+        this.first = first;
+        this.second = second;
+    }
+}
