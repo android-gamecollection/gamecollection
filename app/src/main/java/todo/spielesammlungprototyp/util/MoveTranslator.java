1f36cda6f3622ee5d2bde61fd5611581463838b1
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-05-23 09:43:44 +0200

    Replace map initialization by MapBuilder().build()

--- app/src/main/java/todo/spielesammlungprototyp/util/MoveTranslator.java
+++ app/src/main/java/todo/spielesammlungprototyp/util/MoveTranslator.java
@@ -1,6 +1,5 @@
 package todo.spielesammlungprototyp.util;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,26 +12,26 @@ public class MoveTranslator {
     private Map<Integer, String> verticalmap;
 
     private MoveTranslator() {
-        horizontalmap = new HashMap<>();
-        verticalmap = new HashMap<>();
-
-        horizontalmap.put(0, "A");
-        horizontalmap.put(1, "B");
-        horizontalmap.put(2, "C");
-        horizontalmap.put(3, "D");
-        horizontalmap.put(4, "E");
-        horizontalmap.put(5, "F");
-        horizontalmap.put(6, "G");
-        horizontalmap.put(7, "H");
-
-        verticalmap.put(0, "8");
-        verticalmap.put(1, "7");
-        verticalmap.put(2, "6");
-        verticalmap.put(3, "5");
-        verticalmap.put(4, "4");
-        verticalmap.put(5, "3");
-        verticalmap.put(6, "2");
-        verticalmap.put(7, "1");
+        horizontalmap = new MapBuilder<Integer, String>().build(
+                0, "A",
+                1, "B",
+                2, "C",
+                3, "D",
+                4, "E",
+                5, "F",
+                6, "G",
+                7, "H"
+        );
+        verticalmap = new MapBuilder<Integer, String>().build(
+                0, "8",
+                1, "7",
+                2, "6",
+                3, "5",
+                4, "4",
+                5, "3",
+                6, "2",
+                7, "1"
+        );
     }
 
     public static MoveTranslator getInstance() {
