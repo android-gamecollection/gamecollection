82073a72b74a447321d889ed87c37364eaec5e27
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-05-27 00:40:35 +0200

    Format class

--- app/src/main/java/todo/spielesammlungprototyp/Chess/ChessWrapper.java
+++ app/src/main/java/todo/spielesammlungprototyp/Chess/ChessWrapper.java
@@ -6,17 +6,16 @@ import com.alonsoruibal.chess.Move;
 import com.alonsoruibal.chess.search.SearchEngine;
 
 public class ChessWrapper {
+    Board board;
     private Config config;
     private SearchEngine searchengine;
-    Board board;
 
-    public ChessWrapper(){
+    public ChessWrapper() {
         config = new Config();
     }
-    public void setDifficlty(int difficlty)
-    {
-        switch (difficlty)
-        {
+
+    public void setDifficlty(int difficlty) {
+        switch (difficlty) {
             case 3:
                 config.setTranspositionTableSize(100);
                 break;
@@ -28,64 +27,61 @@ public class ChessWrapper {
                 config.setTranspositionTableSize(10);
         }
     }
-    private void initiate()
-    {
+
+    private void initiate() {
         searchengine = new SearchEngine(config);
         board = searchengine.getBoard();
     }
-    public Board getBoard()
-    {
+
+    public Board getBoard() {
         return board;
     }
-    public boolean doMove(String move)
-    {
-        int intmove = Move.getFromString(board,move,true);
-        if(board.doMove(intmove)) {
+
+    public boolean doMove(String move) {
+        int intmove = Move.getFromString(board, move, true);
+        if (board.doMove(intmove)) {
             animation(move);
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
-    public String getBestMove()
-    {
-        try{
+
+    public String getBestMove() {
+        try {
             searchengine.run();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         String string = Move.toString(searchengine.getBestMove());
         return string;
     }
-    public void aimove()
-    {
+
+    public void aimove() {
         doMove(getBestMove());
     }
-    public void restart()
-    {
+
+    public void restart() {
         initiate();
         board.startPosition();
     }
-    public String getFen()
-    {
+
+    public String getFen() {
         return board.getFen();
     }
+
     public String getOverview() {
         String str = board.toString();
         str = str.substring(0, 144); // 144 = amount of chars on a normal field
         str = str + "\n" + "a b c d e f g h";
         return str;
     }
-    public int isEndgame()
-    {
+
+    public int isEndgame() {
         return board.isEndGame();
     }
-    private void animation(String move)
-    {
+
+    private void animation(String move) {
 
     }
 }
