ab6e987bb2352aa926ad742e83c614a88e05139c
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-06-05 02:19:19 +0200

    Outsource loading of games from 'games.xml' in quasi singleton and add class 'App extends Application' to get application context at any time

---
+++ app/src/main/java/todo/spielesammlungprototyp/model/util/Games.java
@@ -0,0 +1,103 @@
+package todo.spielesammlungprototyp.model.util;
+
+import android.content.Context;
+import android.content.res.XmlResourceParser;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import todo.spielesammlungprototyp.App;
+import todo.spielesammlungprototyp.R;
+import todo.spielesammlungprototyp.view.GameCardView;
+
+public final class Games {
+
+    private final static Games instance = new Games();
+    private static final String TAG = instance.getClass().getSimpleName();
+    public final Map<String, List<GameCardView>> games = new HashMap<>();
+    private final String[] XML_ATTRIBUTES = {"icon", "title", "description", "rules", "activity"};
+
+    private Games() {
+        loadGamesFromXml();
+        sortGameList();
+    }
+
+    public synchronized static Map<String, List<GameCardView>> getGameList() {
+        return instance.games;
+    }
+
+    public static void logGameList() {
+        for (Map.Entry<String, List<GameCardView>> entry : getGameList().entrySet()) {
+            Log.d(TAG, entry.getKey() + ":");
+
+            for (GameCardView game : entry.getValue()) {
+                Log.d(TAG, game.toString());
+            }
+        }
+    }
+
+    private void loadGamesFromXml() {
+        Context mContext = App.getContext();
+        try (XmlResourceParser xmlGames = mContext.getResources().getXml(R.xml.games)) {
+            String gameCategory = null;
+
+            int eventType = xmlGames.getEventType();
+            for (; eventType != XmlPullParser.END_DOCUMENT; eventType = xmlGames.next()) {
+                if (eventType != XmlPullParser.START_TAG) continue;
+
+                switch (xmlGames.getDepth()) {
+                    case 2:
+                        gameCategory = xmlGames.getName();
+                        games.put(gameCategory, new ArrayList<GameCardView>());
+                        break;
+                    case 3:
+                        String[] attributes = XML_ATTRIBUTES.clone();
+                        for (int i = 0; i < attributes.length; i++) {
+                            String attributeValue = xmlGames.getAttributeValue(null, attributes[i]);
+                            attributes[i] = dereferenceString(attributeValue);
+                        }
+                        int icon = getResourceIdFromString(attributes[0]);
+                        GameCardView gameCardView = new GameCardView(icon, attributes[1], attributes[2], attributes[3], attributes[4]);
+                        games.get(gameCategory).add(gameCardView);
+                        break;
+                }
+            }
+        } catch (XmlPullParserException | IOException e) {
+            Log.e(TAG, "loadGamesFromXml():", e);
+        }
+    }
+
+    private void sortGameList() {
+        for (Map.Entry<String, List<GameCardView>> entry : games.entrySet()) {
+            Collections.sort(entry.getValue());
+        }
+    }
+
+    /**
+     * Converts a resource from string-form to integer-form
+     *
+     * @param resourceStr The resource as string in the form of '@mimap/ic_launcher'
+     * @return The resource in the form of R.mipmap.ic_launcher
+     */
+    private int getResourceIdFromString(String resourceStr) {
+        Context mContext = App.getContext();
+        if (resourceStr.startsWith("@")) resourceStr = resourceStr.substring(1);
+        String[] splitString = resourceStr.split("/");
+        return mContext.getResources().getIdentifier(splitString[1], splitString[0], mContext.getPackageName());
+    }
+
+    private String dereferenceString(String resourceStr) {
+        Context mContext = App.getContext();
+        if (!resourceStr.startsWith("@string/")) return resourceStr;
+        int identifier = getResourceIdFromString(resourceStr);
+        return mContext.getString(identifier);
+    }
+}
--- app/src/main/java/todo/spielesammlungprototyp/view/fragment/GameSelection.java
+++ app/src/main/java/todo/spielesammlungprototyp/view/fragment/GameSelection.java
@@ -2,28 +2,20 @@ package todo.spielesammlungprototyp.view.fragment;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.XmlResourceParser;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import todo.spielesammlungprototyp.R;
+import todo.spielesammlungprototyp.model.util.Games;
 import todo.spielesammlungprototyp.view.ClickListener;
 import todo.spielesammlungprototyp.view.GameCardView;
 import todo.spielesammlungprototyp.view.GameCardViewAdapter;
@@ -32,10 +24,8 @@ import todo.spielesammlungprototyp.view.activity.GameActivity;
 public class GameSelection extends Fragment implements ClickListener {
 
     private final static String EXTRA_CATEGORY = "category";
-    private static final String[] XML_ATTRIBUTES = {"icon", "title", "description", "rules", "activity"};
     private final String ACTIVITY_PACKAGE = ".view.activity.";
-    private final String TAG = getClass().getSimpleName();
-    private Map<String, List<GameCardView>> games = new HashMap<>();
+    private Map<String, List<GameCardView>> games;
     private String gameCategory;
 
     public static GameSelection newInstance(String category) {
@@ -51,8 +41,7 @@ public class GameSelection extends Fragment implements ClickListener {
         super.onCreate(savedInstanceState);
 
         gameCategory = getArguments().getString(EXTRA_CATEGORY);
-        loadGamesFromXml();
-        sortGameList();
+        games = Games.getGameList();
     }
 
     @Nullable
@@ -83,69 +72,5 @@ public class GameSelection extends Fragment implements ClickListener {
         intent.putExtra(GameActivity.KEY_TITLE, gameCardView.getGameTitle());
         intent.putExtra(GameActivity.KEY_RULES, gameCardView.getGameRules());
         context.startActivity(intent);
-    }
-
-    private void loadGamesFromXml() {
-        try (XmlResourceParser xmlGames = getResources().getXml(R.xml.games)) {
-            String gameCategory = null;
-
-            int eventType = xmlGames.getEventType();
-            for (; eventType != XmlPullParser.END_DOCUMENT; eventType = xmlGames.next()) {
-                if (eventType != XmlPullParser.START_TAG) continue;
-
-                switch (xmlGames.getDepth()) {
-                    case 2:
-                        gameCategory = xmlGames.getName();
-                        games.put(gameCategory, new ArrayList<GameCardView>());
-                        break;
-                    case 3:
-                        String[] attributes = XML_ATTRIBUTES.clone();
-                        for (int i = 0; i < attributes.length; i++) {
-                            String attributeValue = xmlGames.getAttributeValue(null, attributes[i]);
-                            attributes[i] = dereferenceString(attributeValue);
-                        }
-                        int icon = getResourceIdFromString(attributes[0]);
-                        GameCardView gameCardView = new GameCardView(icon, attributes[1], attributes[2], attributes[3], attributes[4]);
-                        games.get(gameCategory).add(gameCardView);
-                        break;
-                }
-            }
-        } catch (XmlPullParserException | IOException e) {
-            Log.e(TAG, "loadGamesFromXml():", e);
-        }
-    }
-
-    private void logGameList() {
-        for (Map.Entry<String, List<GameCardView>> entry : games.entrySet()) {
-            Log.d(TAG, entry.getKey() + ":");
-
-            for (GameCardView game : entry.getValue()) {
-                Log.d(TAG, game.toString());
-            }
-        }
-    }
-
-    private void sortGameList() {
-        for (Map.Entry<String, List<GameCardView>> entry : games.entrySet()) {
-            Collections.sort(entry.getValue());
-        }
-    }
-
-    /**
-     * Converts a resource from string-form to integer-form
-     *
-     * @param resourceStr The resource as string in the form of '@mimap/ic_launcher'
-     * @return The resource in the form of R.mipmap.ic_launcher
-     */
-    private int getResourceIdFromString(String resourceStr) {
-        if (resourceStr.startsWith("@")) resourceStr = resourceStr.substring(1);
-        String[] splitString = resourceStr.split("/");
-        return getResources().getIdentifier(splitString[1], splitString[0], getContext().getPackageName());
-    }
-
-    private String dereferenceString(String resourceStr) {
-        if (!resourceStr.startsWith("@string/")) return resourceStr;
-        int identifier = getResourceIdFromString(resourceStr);
-        return getString(identifier);
     }
 }
