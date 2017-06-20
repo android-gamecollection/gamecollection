4ed9001cbd2f5b8eb86377788661c9e636a449c8
Frank Rügamer <frank.ruegamer@student.fhws.de>
2017-06-20 23:48:07 +0200

    Add ability to set viewHolder content in code

--- app/src/main/java/todo/spielesammlungprototyp/model/games/chess/ChessHistoryAdapter.java
+++ app/src/main/java/todo/spielesammlungprototyp/model/games/chess/ChessHistoryAdapter.java
@@ -1,9 +1,14 @@
 package todo.spielesammlungprototyp.model.games.chess;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +18,7 @@ import todo.spielesammlungprototyp.R;
 public class ChessHistoryAdapter extends RecyclerView.Adapter<ChessHistoryAdapter.ItemViewHolder> {
 
     // Temp item list, will hold moves
+    // TODO: Change to a list of move(tuple)s
     private List<String> mItems = new ArrayList<>();
 
     @Override
@@ -23,7 +29,12 @@ public class ChessHistoryAdapter extends RecyclerView.Adapter<ChessHistoryAdapte
 
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
-
+        String move = mItems.get(position);
+        // TODO: Move to Text
+//        holder.setContent(
+//                R.drawable.game_chess_king_b, "H9", "DD",
+//                R.drawable.game_chess_pawn_b, "H2", "CU"
+//        );
     }
 
     @Override
@@ -45,8 +56,33 @@ public class ChessHistoryAdapter extends RecyclerView.Adapter<ChessHistoryAdapte
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
 
+        private final ImageView leftImage, rightImage;
+        private final TextView leftTextFrom, leftTextTo, rightTextFrom, rightTextTo;
+
         ItemViewHolder(View itemView) {
             super(itemView);
+            LinearLayout viewLeft = (LinearLayout) itemView.findViewById(R.id.move_left);
+            LinearLayout viewRight = (LinearLayout) itemView.findViewById(R.id.move_right);
+            leftImage = (ImageView) viewLeft.findViewById(R.id.image_piece);
+            leftTextFrom = (TextView) viewLeft.findViewById(R.id.text_from);
+            leftTextTo = (TextView) viewLeft.findViewById(R.id.text_to);
+
+            rightImage = (ImageView) viewRight.findViewById(R.id.image_piece);
+            rightTextFrom = (TextView) viewRight.findViewById(R.id.text_from);
+            rightTextTo = (TextView) viewRight.findViewById(R.id.text_to);
+        }
+
+        void setContent(int leftImage, String leftTextFrom, String leftTextTo,
+                        int rightImage, String rightTextFrom, String rightTextTo) {
+            Drawable drawableLeft = ContextCompat.getDrawable(itemView.getContext(), leftImage);
+            this.leftImage.setImageDrawable(drawableLeft);
+            this.leftTextFrom.setText(leftTextFrom);
+            this.leftTextTo.setText(leftTextTo);
+
+            Drawable drawableRight = ContextCompat.getDrawable(itemView.getContext(), rightImage);
+            this.rightImage.setImageDrawable(drawableRight);
+            this.rightTextFrom.setText(rightTextFrom);
+            this.rightTextTo.setText(rightTextTo);
         }
     }
 }
